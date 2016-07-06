9b259d98f8bda5fa2f702b5956c52a5465fe9780
Shivam06 <shivammahajan6596@gmail.com>
2016-07-07 00:35:51 +0530

    Update image.py

--- Loading/image.py
+++ Loading/image.py
@@ -3,6 +3,10 @@ import numpy as np
 import os
 os.chdir(r'C:\Users\SHIVAM MAHAJAN\Desktop')
 img = cv2.imread('messi.jpg',cv2.IMREAD_GRAYSCALE)
+# we can also use cv2.IMREAD_UNCHANGED or cv2.IMREAD_COLOR - which returns colored image without alpha channel.
+# alpha channel represent degree of opaqueness.
+# we can also use -1,0,1 for second parameter. -1 : unchanged, 0 : gray, 1: color.
+
 cv2.imshow("win",img)
 
 cv2.waitKey(0)
